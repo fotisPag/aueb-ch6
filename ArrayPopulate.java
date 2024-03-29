47e1dfbef06c41c1b9a8e8461271028ba14d7050
fotisPag <fotis.pagonis01@gmail.com>
2024-03-29 23:26:14 +0200

    ArrayPopulate project part1

---
+++ ArrayPopulate.java
@@ -0,0 +1,33 @@
+package gr.aueb.cf.ch6;
+
+import java.util.Scanner;
+public class ArrayPopulate {
+
+    public static void main(String[] args) {
+        //aαρχικοποίηση - populate
+        Scanner in = new Scanner(System.in);
+        int[] ages = new int[5];
+
+        for (int i=0; i < ages.length; i++){
+            System.out.println("Δώστε τον αριθμό για την θέση %d" + ages[i]);
+            ages[i] = in.nextInt();
+        }
+
+
+//        int[] ages = {19, 20, 21, 22, 23};
+
+//        int[] ages = new int[5];
+//        ages[0] = 19;
+//        ages[1] = 20;
+//        ages[2] = 21;
+//        ages[3] = 22;
+//        ages[4] = 23;
+
+
+    //εκτύπωση των στοιχείων του πίνακα
+    for (int i = 0; i < ages.length; i++) {
+        System.out.print(ages[i] + " ");
+    }
+        }
+
+}
