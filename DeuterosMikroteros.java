55d16c0f8d5f417cabf46957f80fdec8424a7a48
fotisPag <fotis.pagonis01@gmail.com>
2024-03-31 17:15:20 +0300

    DeuterosMikroteros

---
+++ DeuterosMikroteros.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.ch6;
+
+public class DeuterosMikroteros {
+    public static void main(String[] args) {
+        int[] array1 = {1,2, 3, 4, 5};
+        int minValue = Integer.MAX_VALUE;
+        int minValue2 = Integer.MAX_VALUE;
+        int minPosition = Integer.MAX_VALUE;
+        int secondMinPosition = Integer.MAX_VALUE;
+
+        for (int i = 0; i < array1.length; i++){
+            if (array1[i] < minValue){
+                minValue2 = minValue;
+                secondMinPosition = minPosition;
+                minValue = array1[i];
+                minPosition = i;
+            }else if (array1[i] < minValue2 && array1[i] != minValue2) {
+                minValue2 = array1[i];
+                secondMinPosition = i;
+            }
+        }
+        System.out.println("Ο μικρότερος αριθμός είναι ο " + minValue + " και βρίσκεται στη θέση " + minPosition);
+        System.out.println("Ο δεύτερος μικρότερος αριθμός είναι ο " + minValue2 + " και βρίσκεται στη θέση " + secondMinPosition);
+    }
+}
